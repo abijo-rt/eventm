d2dca970c68448653ebf792e612d167e1e82b21a
unknown <devaabijo@gmail.com>
2024-07-23 23:29:43 +0530

    scound commit

--- event-m/docs/server/main.server.mjs
+++ event-m/docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DBLBGA2X.mjs";import"./chunk-YOG53EF3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MP5X7POB.mjs";import"./chunk-YOG53EF3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
